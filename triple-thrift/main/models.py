7b1807caabe35126ce90cf76c841573d94b2d79a
AlfianFirja <alfian.firja@gmail.com>
2024-09-09 16:55:28 +0700

    please

--- triple-thrift/main/models.py
+++ triple-thrift/main/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 
-class MoodEntry(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     address = models.TextField()
-
-    @property
-    def is_mood_strong(self):
-        return self.mood_intensity > 5
