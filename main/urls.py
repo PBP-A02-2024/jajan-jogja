920a7d2fed50d531f57a7b053b61ccb4f5444d45
szoyaf <shaneyzoyaf@gmail.com>
2024-10-25 15:51:43 +0700

    Revert "Merge pull request #3 from PBP-A02-2024/nabeel"
    This reverts commit 582fd7b8d294bc79b41912267bcc9aa1f5260bec, reversing
    changes made to fa0bd60a3c5c0c66d97f4ba5288a059392a51644.

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, login_user, register, logout_user
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('login/', login_user, name='login_user'),
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout'),
 ]
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
-
+import datetime
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 # Create your views here.
+
 def show_main(request):
-    return render(request, 'index.html')
+    context ={
+        'user':request.user,
+    }
+    return render(request,'index.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+       form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login_user')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def logout_user(request):
+    response = HttpResponseRedirect(reverse('main:show_main'))
+    response.delete_cookie('last_login')
+    logout(request)
+    return response
