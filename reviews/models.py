c20f70fb96c32ca490b5986378f767cabdc77008
Vander Gerald <vander.gerald@ui.ac.id>
2024-10-25 19:57:15 +0700

    Change model Review

--- reviews/models.py
+++ reviews/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from zoya.models import TempatKuliner
-# Create your mode here.
-
+from django.contrib.auth.models import User
+#
 class Review(models.Model):
     RATING_CHOICES = [
         (1, '1 star'),
@@ -11,7 +11,7 @@ class Review(models.Model):
         (5, '5 star')
    ]
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # place = models.ForeignKey()
+    tempat_kuliner = models.ForeignKey(TempatKuliner, null=True, on_delete=models.CASCADE)
     rating = models.IntegerField(choices=RATING_CHOICES)
     comment = models.TextField(null=False,blank=True)
 
