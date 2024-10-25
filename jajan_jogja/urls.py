79c7c5320644f1d56a6d69ebce499acf8985f539
Vander Gerald <vander.gerald@ui.ac.id>
2024-10-25 19:46:11 +0700

    tambah reviews Model

--- jajan_jogja/urls.py
+++ jajan_jogja/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('zoya.urls')),
+    path('review/', include('reviews.urls'))
 ]
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from zoya.models import TempatKuliner
+# Create your mode here.
 
-# Create your models here.
+class Review(models.Model):
+    RATING_CHOICES = [
+        (1, '1 star'),
+        (2, '2 star'),
+        (3, '3 star'),
+        (4, '4 star'),
+        (5, '5 star')
+   ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # place = models.ForeignKey()
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    comment = models.TextField(null=False,blank=True)
+
+
