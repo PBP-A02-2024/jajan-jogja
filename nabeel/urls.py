4efceb49c28dabab2a3c14126a8ddc0cc8336331
beel <nabeelmuhammad1209@gmail.com>
2024-10-25 14:59:59 +0700

    feat:search model implemented

--- nabeel/urls.py
+++ nabeel/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from nabeel.views import search_page
+from nabeel.views import search_by_keyword, search_page, show_search_history, delete_search_history
 
 app_name = 'nabeel'
 
 urlpatterns = [
     path('', search_page, name='search_page'),
+    path('by-keyword/<str:keyword>', search_by_keyword, name="search_by_keyword"),
+    path('show-search-history', show_search_history, name="show_search_history"),
+    path('delete/<str:id>', delete_search_history, name='delete_search_history'),
+
 ]
--- nabeel/views.py
+++ nabeel/views.py
@@ -1,6 +1,59 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.core import serializers
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from django.utils.html import strip_tags
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+
+from nabeel.models import Search
+# import dari punya marco
 
 # Create your views here.
-
+@login_required(login_url='/login')
+@csrf_exempt
 def search_page(request):
-    return render(request, 'search-page.html')
+    context = {
+        'user':request.user,
+        # 'search_history':Search.objects.filter(user=request.user).order_by('-created_at')
+    }
+    if(request.method == "POST"):
+        content = strip_tags(request.POST.get("content"))
+        user = request.user
+        if content:
+            new_search = Search(content=content, user=user)
+            new_search.save()
+            return HttpResponseRedirect(reverse('nabeel:search_by_keyword', kwargs={'keyword':content}))
+    return render(request, 'search-page.html', context)
+
+@csrf_exempt
+def search_by_keyword(request, keyword):
+    # makanan = Makanan.objects.all()
+    context = {
+        'user':request.user,
+        'search_history':Search.objects.filter(user=request.user).order_by('-created_at')
+    }
+    if(request.method == "POST"):
+        content = strip_tags(request.POST.get("content"))
+        user = request.user
+        if content:
+            new_search = Search(content=content, user=user)
+            new_search.save()
+            return HttpResponseRedirect(reverse('nabeel:search_by_keyword', kwargs={'keyword':content}))
+    return render(request, "search-page.html", context)
+
+
+def delete_search_history(request, id):
+    search = Search.objects.get(pk = id)
+    search.delete()
+    return HttpResponse(b"DELETED", status=200)
+
+def show_search_history(request):
+    data = Search.objects.filter(user=request.user).order_by('-created_at')
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+# def search_by_name_or_category(request, name, category):
+#     places =  objects.filter(nama_tempat=name, category=category) (ambil dari punya marco)
+#     context = {'eatery':places}
+#     return HttpResponse(serializers.serialize("json", context), content_type="application/json")
