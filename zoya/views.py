bfc8e116100ebd4a1a60aa56699477426beff97d
szoyaf <shaneyzoyaf@gmail.com>
2024-10-25 11:30:37 +0700

    new updates in landing page, navbar, base and other images added

--- zoya/views.py
+++ zoya/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from zoya.models import TempatKuliner
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
-    
-
     return render(request, 'landing.html')
+
+def show_json_tempat(request):
+    # data = TempatKuliner.objects.filter(user=request.user)
+    data = TempatKuliner.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
