bbf2c416385b3d682d0c674a8069e99775a582d8
beel <nabeelmuhammad1209@gmail.com>
2024-10-25 15:29:05 +0700

    fix:fix url and views

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,6 @@ from main.views import login_user, logout_user, show_main, register
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
     path('login/', login_user, name='login_user'),
     path('register/', register, name='register'),
     path('logout/', logout_user, name='logout'),
