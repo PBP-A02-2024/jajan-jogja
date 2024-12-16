9a3a95fb57a80d223faab6b46ecc40b98091c5a2
Marco <ymarcogracia@gmail.com>
2024-12-16 14:36:06 +0700

    add save food plan flutter

--- marco/views.py
+++ marco/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.shortcuts import redirect, render, get_object_or_404
 from zoya.models import Makanan, TempatKuliner
@@ -6,6 +6,7 @@ from reviews.models import Review
 from reksa.models import FoodPlan
 from django.contrib.auth.decorators import login_required
 import json
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 def get_restaurant(request, tempatKulinerId):
@@ -66,3 +67,45 @@ def save_food_plan(request):
 
     # Jika method GET
     return render(request, 'index.html')
+
+@csrf_exempt
+@login_required(login_url='main:login')
+def save_food_plan_flutter(request):
+    if request.method == 'POST':
+        try:
+            current_resto = request.POST.get('currentResto')
+            resto_obj = TempatKuliner.objects.get(id=current_resto)
+
+            makanan_id = request.POST.get('makanan_id')
+            makanan_obj = Makanan.objects.get(id=makanan_id)   
+            
+            food_plans_data = json.loads(request.POST.get('foodPlansData', '[]'))
+            selected_plan_ids = [plan['id'] for plan in food_plans_data if plan['checked']]
+            unselected_plan_ids = [plan['id'] for plan in food_plans_data if not plan['checked']]
+
+            # Tambah relasi makanan dengan foodplan
+            for plan_id in selected_plan_ids:
+                food_plan = FoodPlan.objects.get(id=plan_id)
+                if not food_plan.makanan.filter(id=makanan_obj.id).exists():
+                    food_plan.tempat_kuliner.add(resto_obj)
+                    food_plan.makanan.add(makanan_obj)
+
+            # Hapus relasi jika sudah ada relasi makanan dan checkbox tidak dicentang
+            for plan_id in unselected_plan_ids:
+                food_plan = FoodPlan.objects.get(id=plan_id)
+                food_plan.check
+                if food_plan.makanan.filter(id=makanan_obj.id).exists():
+                    food_plan.makanan.remove(makanan_obj)
+
+            for plan_id in selected_plan_ids + unselected_plan_ids:
+                food_plan = FoodPlan.objects.get(id=plan_id)
+                if not food_plan.makanan.filter(tempat_kuliner=resto_obj).exists():
+                    # Hapus restoran jika tidak ada relasi ke makanan
+                    food_plan.tempat_kuliner.remove(resto_obj)  
+
+            return JsonResponse({'message': 'Food plan updated successfully!'}, status=200)
+        
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid method'}, status=405)
