f5a1eaa802292091030084c8b77c9c823bc455db
farrelreksa <farrelreksa@gmail.com>
2024-12-20 00:02:28 +0700

    feat: routing for food plan

--- reksa/views.py
+++ reksa/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def food_plans_list(request):
     food_plans = FoodPlan.objects.filter(user=request.user)
     for food_plan in food_plans:
@@ -20,6 +21,7 @@ def food_plans_list(request):
 
 
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def food_plan_detail_view(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
     restaurants = list(food_plan.tempat_kuliner.all())
@@ -43,6 +45,7 @@ def food_plan_detail_view(request, food_plan_id):
     return render(request, 'food_plan.html', context)
 
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def food_plan_create(request):
     new_food_plan = FoodPlan.objects.create(nama="Food Plan", user=request.user)
     return redirect('reksa:food_plan_detail_view', food_plan_id=new_food_plan.id)
@@ -54,22 +57,26 @@ def food_plan_create_json(request):
     return JsonResponse({'food_plan_id': new_food_plan.id})
 
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def food_plan_json(request):
     food_plans = FoodPlan.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", food_plans), content_type="application/json")
 
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def food_plan_detail_json(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
     return HttpResponse(serializers.serialize("json", [food_plan]), content_type="application/json")
 
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def add_food_plan_item(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
     food_plan.makanan.add(request.POST.get('food_item_id'))
     return redirect('reksa:food_plan_detail_view', food_plan_id=food_plan.id)
 
 @login_required(login_url='main:login_user')
+@csrf_exempt
 def remove_food_plan_item(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
     food_plan.makanan.remove(request.POST.get('food_item_id'))
@@ -91,10 +98,15 @@ def update_food_plan_title(request, food_plan_id):
     return JsonResponse({'success': "False"})
 
 @login_required(login_url='main:login_user')
-@require_http_methods(["DELETE"])
+@csrf_exempt
+@require_http_methods(["POST", "DELETE"])
 def remove_food_plan_item(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
+    print("REQUEST BODY")
+    print(request.body)
     data = json.loads(request.body)
+    print("DATA")
+    print(data)
     food_item_id = data.get('food_item_id')
     if food_item_id:
         food_plan.makanan.remove(food_item_id)
@@ -102,18 +114,19 @@ def remove_food_plan_item(request, food_plan_id):
     return JsonResponse({'success': "False"})
 
 @login_required(login_url='main:login_user')
-@require_http_methods(["DELETE"])
+@csrf_exempt
+@require_http_methods(["POST", "DELETE"])
 def remove_food_plan_restaurant(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
     data = json.loads(request.body)
     restaurant_id = data.get('restaurant_id')
     if restaurant_id:
         food_plan.tempat_kuliner.remove(restaurant_id)
-        return JsonResponse({'success': True})
-    return JsonResponse({'success': False})
+        return JsonResponse({'success': "True"})
+    return JsonResponse({'success': "False"})
 
 @login_required(login_url='main:login_user')
-@require_http_methods(["DELETE"])
+@require_http_methods(["POST", "DELETE"])
 def remove_food_plan(request, food_plan_id):
     food_plan = get_object_or_404(FoodPlan, pk=food_plan_id)
     food_plan.delete()
